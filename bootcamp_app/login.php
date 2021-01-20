a80d843b028fe6ae70c0287f11246c1b985e11c4
NaurisV7 <valainis.nauris@gmail.com>
2021-01-20 19:39:06 +0200

    login added

---
+++ bootcamp_app/login.php
@@ -0,0 +1,7 @@
+<form class="style">
+    <label for="username"> Username:</label>
+    <input type="username" id="username">
+    <label for="password">Password:</label>
+    <input type="password" id="password">    
+    <button type="submit">Submit</button>
+</form>
---
+++ public/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include "../bootcamp_app/page404.php"; ?>
+    <?php include "../bootcamp_app/login.php"; ?>
+    <?php echo "Hello world"?>
+</body>
+</html>
+
+
